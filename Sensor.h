30fe91a8bbaabfd71a934b3d34fa72596dc033fc
jasonleethomas <jsnleethms@gmail.com>
2015-02-16 13:47:34 -0500

    Bug fixes

--- Sensor.h
+++ Sensor.h
@@ -1,5 +1,8 @@
-#ifndef Sensor
-#define Sensor
+#include <stdint.h>
+#include <Arduino.h>
+
+#ifndef SENSOR_H
+#define SENSOR_H
 
 class Sensor {
 private:
@@ -8,7 +11,7 @@ private:
 
 	uint8_t bit;
 	uint8_t pin;
-	uint8_t emmiterPin;
+	uint8_t emitterPin;
 
 public:
 	Sensor();
@@ -26,38 +29,38 @@ public:
 
 Sensor::Sensor() {
 	this->threshold = 0;
-	this->bit = B00000000;
+	this->bit = 0;
 	this->val = 0;
-	this->pin = NULL;
-	this->emitterPin = NULL;
+	this->pin = 0;
+	this->emitterPin = 0;
 }
 
-Sensors::Sensor(uint8_t pin) {
+Sensor::Sensor(uint8_t pin) {
 	this->threshold = 0;
-	this->bit = B00000000;
+	this->bit = 0;
 	this->val = 0;
 	this->attach(pin);
 }
 
-Sensors::Sensor(uint8_t pin, uint8_t emitterPin) {
+Sensor::Sensor(uint8_t pin, uint8_t emitterPin) {
 	this->threshold = 0;
-	this->bit = B00000000;
+	this->bit = 0;
 	this->val = 0;
 	this->attach(pin, emitterPin);
 }
 
 
 /*	If pin index is within range attaches Sensor variable
-		is attached pin, and returns 1. Returns 0 otherwise
-		and pin is set to NULL.
-		Emitter is defaulted to NULL.
+	is attached pin, and returns 1. Returns 0 otherwise
+	and pin is set to 0.
+	Emitter is defaulted to 0.
  */
 uint8_t Sensor::attach(uint8_t  pin) {
-	this->emitterPin = NULL;
+	this->emitterPin = 0;
 
 	// analogRead() can only be called on pins 14-19 (A0-A5).
 	if(pin < 14 || pin > 19) {
-		this->pin = NULL;
+		this->pin = 0;
 		return 1;
 	}
 
@@ -68,15 +71,15 @@ uint8_t Sensor::attach(uint8_t  pin) {
 
 uint8_t Sensor::attach(uint8_t pin, uint8_t emitterPin) {
 	if(pin < 14 || pin > 19) {
-		this->pin = NULL;
+		this->pin = 0;
 		return 1;
 	}
 
 	/*	digitalWrite() can only be called on pins 0-19.
-			avoids using Sensor pin as emitterPin		
+		avoids using Sensor pin as emitterPin		
 	*/
 	if(emitterPin < 0 || emitterPin > 19 || emitterPin == this->pin) {
-		this->emitterPin = NULL;
+		this->emitterPin = 0;
 		return 1;
 	}
 
@@ -87,17 +90,17 @@ uint8_t Sensor::attach(uint8_t pin, uint8_t emitterPin) {
 }
 
 uint8_t Sensor::detach() {
-	this->pin = NULL;
-	this->emitterPin = NULL;
+	this->pin = 0;
+	this->emitterPin = 0;
 }
 
 /*	Maximum and minimum values of sensor is found.
-		Readings are adjusting for ambient reflections,
-		and used to obtain threshold.
-		If calibrated returns true, else false.
+	Readings are adjusting for ambient reflections,
+	and used to obtain threshold.
+	If calibrated returns true, else false.
  */
 
-uint8_t Sensors::calibrate() {
+uint8_t Sensor::calibrate() {
 	if(!this->pin)
 		return 1;
 
@@ -122,13 +125,13 @@ uint8_t Sensors::calibrate() {
 }
 
 /*	Reads analog value from the "sensor" IR emitter in ON state. 
-		A second readings is taken with the IR emitters in OFF state. 
-		This method of reading sensors reduces the effect of 
-		ambient light. If the analog value for sensor is above its
- 		threshold, then a binary 1 is written. In all other case a binary
-		0 is written.	
+	A second readings is taken with the IR emitters in OFF state. 
+	This method of reading sensors reduces the effect of 
+	ambient light. If the analog value for sensor is above its
+	threshold, then a binary 1 is written. In all other case a binary
+	0 is written.	
  */
-uint16_t Sensors::read() {
+uint16_t Sensor::read() {
 	if(!this->pin)
 		return this->val;
 
@@ -145,7 +148,7 @@ uint16_t Sensors::read() {
 
 	// take another with emitters off
 	digitalWrite(this->emitterPin, LOW);	
-	ambientVal = analogRead(sensor);
+	ambientVal = analogRead(this->pin);
 
 	// adjust reading for ambient reflections
 	this->val -= ambientVal;	
@@ -159,9 +162,9 @@ uint16_t Sensors::read() {
 }
 
 /*	Returns bit representation of Sensor value.
-		If the analog value for sensor is above its	threshold,
-		then a binary 1 is written. In all other case a binary
-		0 is written.	
+	If the analog value for sensor is above its	threshold,
+	then a binary 1 is written. In all other case a binary
+	0 is written.	
 	*/
 uint8_t Sensor::readBit() {
 	this->read();
