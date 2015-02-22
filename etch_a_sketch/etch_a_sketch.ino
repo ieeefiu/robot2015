2efed55b3efbd9ceac52e5545e8cd6392a832449
William Oberndorfer <will.oberndorfer@gmail.com>
2015-02-22 14:02:42 -0500

    servo control hello world

--- etch_a_sketch/etch_a_sketch.ino
+++ etch_a_sketch/etch_a_sketch.ino
@@ -1,3 +1,25 @@
 #include <Servo.h>
 
 
+Servo left_knob;
+// Servo right_knob;
+int position = 0;
+
+void setup
+{
+    left_knob.attach(A0);
+    // right_knob.attach(A1);
+}
+
+void loop
+{
+    for(pos = 0; pos < 180; pos++) {
+	left_knob.write(pos);
+	delay(15);
+    }
+
+    for(pos = 180; pos >= 1; pos--) {
+	left_knob.write(pos);
+	delay(15);
+    }
+}
