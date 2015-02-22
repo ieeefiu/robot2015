e706ee57ddc36ea902ae7022114293814cc14d19
William Oberndorfer <will.oberndorfer@gmail.com>
2015-02-22 14:04:10 -0500

    servo hello world

--- etch_a_sketch/etch_a_sketch.ino
+++ etch_a_sketch/etch_a_sketch.ino
@@ -5,13 +5,13 @@ Servo left_knob;
 // Servo right_knob;
 int position = 0;
 
-void setup
+void setup()
 {
     left_knob.attach(A0);
     // right_knob.attach(A1);
 }
 
-void loop
+void loop()
 {
     for(pos = 0; pos < 180; pos++) {
 	left_knob.write(pos);
